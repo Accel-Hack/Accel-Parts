67243ffb2664c9851e429e9fc7d2d29fae78eb74
Kengo Watanabe <kengo022348@gmail.com>
2022-12-09 12:22:38 +0900

    RequestをResponseに修正

--- js-http/src/main/http/entity/Response.ts
+++ js-http/src/main/http/entity/Response.ts
@@ -1,34 +1,34 @@
 import ResponseError from "./ResponseError";
 
-type RequestProps<E> = {
+type ResponseProps<E> = {
   operationKey: number
   operationStatus: boolean
   result: E | undefined
   errors: ResponseError[] | undefined
 }
 
-class Request<E> {
+class Response<E> {
   readonly operationKey: number
   readonly operationStatus: boolean
   readonly result: E | undefined
   readonly errors: ResponseError[] | undefined
 
-  private constructor(props: RequestProps<E>) {
+  private constructor(props: ResponseProps<E>) {
     this.operationKey = props.operationKey
     this.operationStatus = props.operationStatus
     this.errors = props.errors
     this.result = props.result
   }
 
-  static decode<E>(object: any): Request<E> {
+  static decode<E>(object: any): Response<E> {
     const props = {
       operationKey: object.operationKey,
       operationStatus: object.operationStatus,
       result: object.result as E, // FIXME: ここでclassにdecodeしたい
       errors: object.errors,
-    } as RequestProps<E>
-    return new Request(props)
+    } as ResponseProps<E>
+    return new Response(props)
   }
 }
 
-export default Request
+export default Response
